8c0e49ec61df7d3197c4f3779bc924fa710ea395
rohit-3003 <rohitshirsat100@gmail.com>
2026-03-24 16:44:16 +0530

    Updated live backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR-APP-NAME.onrender.com/api', // Update this after deploying the backend!
+  apiUrl: 'https://study-platform-backend-ii83.onrender.com', // Update this after deploying the backend!
   razorpayKeyId: 'rzp_live_xxxxxxxxxxxx'
 };
